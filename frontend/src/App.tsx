82187ada630c4081774cd41f7d0e9ba424daa8b7
mhoufani <m.houfani.dev@gmail.com>
2025-06-05 15:50:36 +0200

    fix(frontend): fix router issue to navigate tospaceship-admin page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,7 +24,7 @@ export function App() {
       element: <Cockpit />,
     },
     {
-      path: '/spaceship-admin-broken',
+      path: '/spaceship-admin',
       element: <SpaceshipAdmin />,
     },
     {
